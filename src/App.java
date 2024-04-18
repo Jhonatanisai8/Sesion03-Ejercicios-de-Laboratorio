4a53dca389df8671420c22ef9d91317bf645c526
jho-my <ojedasnachezjhonatan@gmail.com>
2024-04-18 09:34:53 -0500

    llamada al Metodo quicShort desde la clase main

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import PrimerEjercicio.Alumno;
+import PrimerEjercicio.Quick;
 
 public class App {
     /*
@@ -11,19 +12,25 @@ public class App {
 
         // ingresamos elementos al arreglo del alumnos
         Alumno[] array = new Alumno[3]; // de tres posiciones
-        //asiganamos valores
+        // asiganamos valores
         array[0] = new Alumno("41742587", "Juan Perez", 21, 14);
-        array[1] = new Alumno("10587458", "Diego Liviano",27, 11);
+        array[1] = new Alumno("10587458", "Diego Liviano", 27, 11);
         array[2] = new Alumno("52962858", "Vania Salcedo", 17, 12);
 
         System.out.println("Datos Antes del ordenamiento");
         mostrar(array);
+
+        // creamos un obejto de la clase quickShort y llamamos al metodo
+        Quick obj = new Quick();
+        obj.ordenar(array);
+        System.out.println("Datos despues del Ordenamiento: ");
+        mostrar(array);
     }
 
-    public static void mostrar(Alumno[] arreglo){
+    public static void mostrar(Alumno[] arreglo) {
         int total = arreglo.length;
         for (int i = 0; i < total; i++) {
-            System.out.println(i+" "+arreglo[i].mostrarDatos());
+            System.out.println(i + " " + arreglo[i].mostrarDatos());
         }
         System.out.println();
     }
